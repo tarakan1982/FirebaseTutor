422527cd8a81eb1e73c3f926f54ccdfce1e9b260
Dmitriy Borisov <dm.borisov1982@gmail.com>
2020-12-25 15:09:48 +0300

    Initial Commit

---
+++ FirebaseTutorial/FirebaseTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTutorialApp.swift
+//  FirebaseTutorial
+//
+//  Created by Dmitriy Borisov on 25.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
